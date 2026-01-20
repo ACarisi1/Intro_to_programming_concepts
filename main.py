37ea47be3a6ba46d17df0b5fa771307c3b789db4
ACarisi1 <acarisi@mail.valenciacollege.edu>
2026-01-20 17:56:44 -0500

    second class notes

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 print("Adam Carisi")
+#using the pound/hashtag symbol is a way to do comments.
+print('hello how are you today?')
+
+name = "Scooby" #variables unlike other languages don't have a type.
+
+#int, double, float, char, string, boolean.
+#int is integer
+#float is a decimal, accurate up to seven positions
+#double is double the float about 14-15
+#char stands for character
+#string represents words
+#boolean True/False
+#== is equals = is gets or asigns to.
+age = 23 #letter and characters need the double quotes not numbers
+temperature = 98.7
+
+#types of printing
+print("I am {} years old, and the temperature is {} degrees".format(age, temperature))
+
+#input
+friendName = input("What is my name?\n") #\n next line \s space \tab tab
+print("The new friend name is" , friendName)
+fage = int(input("What is your favorite age?\n")) #python will default to strings other forms have to be declared.
+print (fage)
+
+total = 52 + 78
+print (total)
+
+agesTotal = fage + age
+print (agesTotal)
+
+modulusTotal = 5 % 2
+print (modulusTotal)
