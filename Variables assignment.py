6010a86d1d14340c3a6da4414bd40018b1153e7b
ACarisi1 <acarisi@mail.valenciacollege.edu>
2026-01-20 18:24:49 -0500

    Start of variables assignment

---
+++ Variables assignment.py
@@ -0,0 +1,31 @@
+multiple = int(input("Enter a number: \n"))
+multiple2 = int(input("Enter another number: \n"))
+MultipleAns = multiple*multiple2
+print("The product is", MultipleAns)
+
+divide = int(input("Enter a number: \n"))
+divide2 = int(input("Enter another number: \n"))
+divideAns = divide/divide2
+print("The quotient is", divideAns)
+
+divideWithout = int(input("Enter a number: \n"))
+divideWithout2 = int(input("Enter another number: \n"))
+divideWithoutAns = divideWithout//divideWithout2
+print("The answer is", divideWithoutAns)
+
+divideWIth = int(input("Enter a number: \n"))
+divideWIth2 = int(input("Enter another number: \n"))
+divideWIth = divideWIth%divideWIth2
+print("the remainder is", divideWIth)
+
+sum = int(input("Enter a number: \n"))
+sum2 = int(input("Enter another number: \n"))
+sumAns = sum + sum2
+print("The sum is", sumAns)
+
+difference = int(input("Enter a number: \n"))
+difference2 = int(input("Enter another number: \n"))
+differenceAns = difference - difference2
+print("The difference is", differenceAns)
+
+print("Adam Carisi")
