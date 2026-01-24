58fd7a1b8e3b9994787ab88ece2dfe338f5205db
ACarisi1 <acarisi@mail.valenciacollege.edu>
2026-01-24 12:35:29 -0500

    user changes

--- Variables assignment.py
+++ Variables assignment.py
@@ -1,30 +1,36 @@
-multiple = int(input("Enter a number: \n"))
-multiple2 = int(input("Enter another number: \n"))
+print("Enter two numbers to multiply")
+multiple = int(input("Enter first number: \n"))
+multiple2 = int(input("Enter second number: \n"))
 multipleAns = multiple*multiple2
 print("The answer to your query is: ", multipleAns)
 
-divide = int(input("Enter a number: \n"))
-divide2 = int(input("Enter another number: \n"))
+print("Enter two numbers to divide")
+divide = int(input("Enter first number: \n"))
+divide2 = int(input("Enter second number: \n"))
 divideAns = divide/divide2
 print("The answer to your query is: ", divideAns)
 
-divideWithout = int(input("Enter a number: \n"))
-divideWithout2 = int(input("Enter another number: \n"))
+print("Enter two number to divide to a whole number")
+divideWithout = int(input("Enter first number: \n"))
+divideWithout2 = int(input("Enter second number: \n"))
 divideWithoutAns = divideWithout//divideWithout2
 print("The answer to your query is: ", divideWithoutAns)
 
-divideWIth = int(input("Enter a number: \n"))
-divideWIth2 = int(input("Enter another number: \n"))
+print("Enter two number to show the remainder")
+divideWIth = int(input("Enter first number: \n"))
+divideWIth2 = int(input("Enter second number: \n"))
 divideWIth = divideWIth%divideWIth2
 print("The answer to your query is: ", divideWIth)
 
-sum = int(input("Enter a number: \n"))
-sum2 = int(input("Enter another number: \n"))
+print("Enter two numbers to add")
+sum = int(input("Enter first number: \n"))
+sum2 = int(input("Enter second number: \n"))
 sumAns = sum + sum2
 print("The answer to your query is: ", sumAns)
 
-difference = int(input("Enter a number: \n"))
-difference2 = int(input("Enter another number: \n"))
+print("Anter two numbers to subract")
+difference = int(input("Enter first number: \n"))
+difference2 = int(input("Enter second number: \n"))
 differenceAns = difference - difference2
 print("The answer to your query is: ", differenceAns)
 
